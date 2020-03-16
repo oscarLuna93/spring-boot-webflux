27c48279810fc70e0cc858cb1c7d4f07437d9f42
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-15 23:09:16 -0600

    datos default

--- src/main/java/com/oscar/springboot/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/oscar/springboot/app/SpringBootWebfluxApplication.java
@@ -1,13 +1,36 @@
 package com.oscar.springboot.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootWebfluxApplication {
+import com.oscar.springboot.app.models.dao.ProductoDao;
+import com.oscar.springboot.app.models.documents.Producto;
 
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootWebfluxApplication implements CommandLineRunner{
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+	
+	@Autowired
+	ProductoDao productoDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Flux.just(new Producto("Tv pantalla", 453.12),
+				new Producto("Camara Sony", 123.2))
+		.flatMap(producto -> productoDao.save(producto))
+		.subscribe(producto -> log.info("insert: " + producto.getId())) ;
 	}
 
 }
